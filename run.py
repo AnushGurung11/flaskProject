7c816b217e1170cc867219385aa496141db791f4
AnushGurung11 <anushgrg15@gmail.com>
2026-07-10 12:08:54 +0545

    Remove unused flask_restful import

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from flask_restful import Api
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
